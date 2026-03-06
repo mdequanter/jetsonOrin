f00f7a25567444576b19fa4dad88ec314134b075
Maarten Dequanter <74420584+mdequanter@users.noreply.github.com>
2026-03-06 15:18:25 +0100

    Update signalingServer.py

--- signalingServer.py
+++ signalingServer.py
@@ -49,6 +49,8 @@ def room_from_path(path: str) -> str:
 
 async def process_request(path, request):
     auth = request.headers.get("Authorization")
+
+    print (f"🔐 Incoming connection with Authorization: {auth}")
     if not auth or not auth.startswith("Bearer "):
         return (401, [], b"Missing/invalid Authorization header")
 

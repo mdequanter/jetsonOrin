fedb385696ee1c8e390a47d1127efcdf0b37a237
Maarten Dequanter <74420584+mdequanter@users.noreply.github.com>
2026-03-19 08:46:28 +0100

    segmentJetson

--- segmentVideo.py
+++ segmentVideo.py
@@ -147,7 +147,7 @@ async def receive_and_infer():
                 dy = start_y - target_y
                 direction_angle = float(np.degrees(np.arctan2(dy, dx)))
 
-            #print(f"Frame ID: {frame_id}, Direction Angle: {direction_angle:.2f}°")
+            print(f"Frame ID: {frame_id}, Direction Angle: {direction_angle:.2f}°")
             await ws.send(json.dumps({"heading": round(direction_angle, 2), "frame_id": frame_id}))
 
 asyncio.run(receive_and_infer())

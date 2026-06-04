f78381a34197353f922c2043279d8d6320db1d3c
Maarten Dequanter <74420584+mdequanter@users.noreply.github.com>
2026-06-04 20:33:51 +0200

    Update keyboard2.py

--- Unitree/keyboard2.py
+++ Unitree/keyboard2.py
@@ -42,7 +42,7 @@ x : stretch
 y : Sit / t: Rize sit
 f : Scrape
 g : Front Jump
-h : Hand stand
+j : Hand stand
 
 m : moonwalk (NON FUNCTIONAL)
 -----------------
@@ -178,7 +178,7 @@ def keyboard_listener(conn, loop, stop_event):
                     RTC_TOPIC["SPORT_MOD"],
                     {"api_id": 1031, "parameter": {"data": False}}
                 ), loop)
-        elif k == "h": # Hand stand
+        elif k == "j": # Hand stand
             asyncio.run_coroutine_threadsafe(
                 conn.datachannel.pub_sub.publish_request_new(
                     RTC_TOPIC["SPORT_MOD"],
